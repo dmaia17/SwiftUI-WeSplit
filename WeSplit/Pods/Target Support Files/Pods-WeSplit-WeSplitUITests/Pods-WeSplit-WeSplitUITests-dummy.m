3f817cfba3f599f2872f964cfd5e1177893c1540
danielbs2 <daniel.passos@bs2tecnologia.com.br>
2022-04-19 10:28:27 -0300

    add project

---
+++ WeSplit/Pods/Target Support Files/Pods-WeSplit-WeSplitUITests/Pods-WeSplit-WeSplitUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WeSplit_WeSplitUITests : NSObject
+@end
+@implementation PodsDummy_Pods_WeSplit_WeSplitUITests
+@end
---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Daniel Maia dos Passos on 19/04/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
